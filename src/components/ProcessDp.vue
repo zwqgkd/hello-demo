950ae58d9af56ee894396fa67de74e85a2bf80b6
zwqgkd <2642253423@qq.com>
2022-09-28 20:07:36 +0800

    1

--- src/components/ProcessDp.vue
+++ src/components/ProcessDp.vue
@@ -3,13 +3,15 @@
         <el-tab-pane v-for="item in editableTabs" :key="item.name" :label="item.title" :name="item.name">
             <el-container v-html=item.content style="width: 100%; height: 100%; "></el-container>
         </el-tab-pane>
+        <!-- <ToolBar></ToolBar> -->
     </el-tabs>
+    
 </template>
 
 
 <script setup>
     import { tabIndex, editableTabsValue, editableTabs, handleTabsEdit } from '../js/process'
-import ToolBar from './ToolBar.vue';
+// import ToolBar from './ToolBar.vue';
 
 
 </script>
