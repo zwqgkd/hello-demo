e10112bfc42c8f537ce700ae50eaf0bb87f0d0df
zwqgkd <2642253423@qq.com>
2022-10-12 09:22:58 +0800

    流程图尺寸有问题，固定height 800px

--- src/流程图界面/ProcessDp.vue
+++ src/流程图界面/ProcessDp.vue
@@ -1,7 +1,7 @@
 <template>
     <el-tabs v-model="editableTabsValue" type="card" editable class="demo-tabs" @edit="handleTabsEdit">
         <el-tab-pane v-for="item in editableTabs" :key="item.name" :label="item.title" :name="item.name">
-            <el-container v-html=item.content style="width: 100%; height: 100%; "></el-container>
+            <el-container v-html=item.content style="width: 100%; height: 800px; "></el-container>
         </el-tab-pane>
         <!-- <ToolBar></ToolBar> -->
     </el-tabs>
