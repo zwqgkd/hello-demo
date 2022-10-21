e8801f1acb117514fe1c44e037b9161cdf59f1fb
zwqgkd <2642253423@qq.com>
2022-10-21 12:36:25 +0800

    分离了js

--- src/主界面/mainWindow.js
+++ src/主界面/mainWindow.js
@@ -1,11 +1,40 @@
 
-import {jump} from '../../sys/jump'
+import {ref} from "vue";
+import ProcessDp from "@/流程图界面/ProcessDp.vue";
+import ImageArea from "@/图片展示区/ImageArea.vue";
+import ResultArea from "@/结果展示区/ResultArea.vue";
+import LayoutOne from "@/主界面/components/layout/LayoutOne.vue";
+import LayoutTwo from "@/主界面/components/layout/LayoutTwo.vue";
+import LayoutThree from "@/主界面/components/layout/LayoutThree.vue";
 
-function onMenuClick(message){
-    jump(message)
+//右半部分自适应高度
+const height_right=ref(0)
+height_right.value=window.innerHeight-82
+function changeRightHeight(){
+    height_right.value=window.innerHeight-82
 }
 
-export {onMenuClick}
+
+//动态布局
+const compnts = ref([
+    ProcessDp,
+    ImageArea,
+    ResultArea
+])
+const mainLayout=ref(LayoutOne)
+function layout(i) {
+    switch (i) {
+        case 1:
+            mainLayout.value=LayoutTwo
+            break;
+        case 2:
+            mainLayout.value=LayoutThree
+            break;
+        default:
+            mainLayout.value=LayoutOne
+    }
+}
+export {changeRightHeight,height_right,mainLayout,layout,compnts}
 
 
 
